3572d382d46b20fd9fb8873623d640a900d46c9b
Jennifer Endisch <Jennifer Endisch>
2017-04-05 10:30:32 -0500

    Moving HTML and JS files into folder

---
+++ DiceGame.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+
+/*Task: Write a program that checks if two strings are an anagram of each other.
+*/
+			
+
+function 			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
