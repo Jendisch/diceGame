1b5dbe1b272cf4631640cecdecc0ee84d5ef3c88
Jennifer Endisch <Jennifer Endisch>
2017-04-05 10:48:10 -0500

    Modifying bases of HTML, CSS, and JS files

--- DiceGame.js
+++ DiceGame.js
@@ -2,7 +2,10 @@
 
 
 
-/*Task: Write a program that checks if two strings are an anagram of each other.
+/*Task: Dice Game. Design and write a game that uses at least one each of the 
+		following dice, 4 sides, 6 sides, 8 sides, 10 sides, 12 sides, and 20 sides. 
+		The rules and design are up to you , but the game must involve some sort 
+		of user interaction, and the rules must be more complicated than war.
 */
 			
 
