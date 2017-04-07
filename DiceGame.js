1088bc7c4eca8cd5e121755889bc029321435c91
Jennifer Endisch <Jennifer Endisch>
2017-04-07 11:51:13 -0500

    CSS additions and making the game prettier.

--- DiceGame.js
+++ DiceGame.js
@@ -18,8 +18,8 @@ function introPlayer (){
 }
 
 function introRules(playerName){							
-	document.getElementById("introRules1").innerHTML = "The rules are as follows: <br> You will soon be asked to roll a 6-sided die to determine what evil computer character you will be facing. After your opponent is determined, Terrible Twos will begin! <br> The goal of the game is to reach 50 points before your opponent does. You will do this through a series of choices. Your choices are: Four, Eight, Ten, Twelve, or Twenty.<ul> <li>By choosing Four: you will roll two 4-sided dice. If either of the dice are a 2, no points will be added and your score will decrease by 2. If you roll doubles, your sum of dice will double and be added to your score (i.e. roll two 3's, their sum is 6, 6 is doubled and 12 points are added to your score). There is no penalty for double 2's other than score decreasing by 2. Otherwise, sum of dice will be added to your total score (i.e. roll a 1 and 4, 5 points are added to your score).</li> <li> By choosing Eight: you will roll an 8-sided die. If you roll any 2s, no points will be added and your score will decrease by 4. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 7, 7 points are added to your score).</li> <li> By choosing Ten:	you will roll a 10-sided die. If you roll any 2s, no points will be added and your score will decrease by 5. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 9, 9 points are added to your score).</li> <li>By choosing Twelve: you will roll a 12-sided die. If you roll any 2s, no points will be added and your score will decrease by 5. Keep in mind, 12 has a 2 in it! If you roll either a 2 or a 12 your score will decrease by 5. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 11, 11 points are added to your score).</li> <li>By choosing Twenty: you will roll a 20-sided die. If you roll a 2, no points will be added and your score will decrease by 6, Keep in mind, 12 and 20 both have 2s in them! If you roll either a 2, 12 or 20 your score will decrease by 6. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 16, 16 points are added to your score).</li> </ul> You and the computer will take turns making a choice, and you will be up first. If you happen to reach 50 first, your opponent is allowed one last chance.";						
-	document.getElementById("introRules2").innerHTML = "I hope you're all ready to get started! Click the 'Choose your opponent' button above on the right.";												
+	document.getElementById("introRules1").innerHTML = "<h3><u>The rules are as follows:</u></h3> You will soon be asked to roll a 6-sided die to determine what evil computer character you will be facing. After your opponent is determined, Terrible Twos will begin! <br> The goal of the game is to reach 100 points before your opponent does. You will do this through a series of choices. Your choices are: Four, Eight, Ten, Twelve, or Twenty.<ul> <li><b>By choosing Four</b>: you will roll two 4-sided dice. If either of the dice are a 2, no points will be added and your score will decrease by 2. If you roll doubles, your sum of dice will double and be added to your score (i.e. roll two 3's, their sum is 6, 6 is doubled and 12 points are added to your score). There is no penalty for double 2's other than score decreasing by 2. Otherwise, sum of dice will be added to your total score (i.e. roll a 1 and 4, 5 points are added to your score).</li> <li> <b>By choosing Eight</b>: you will roll an 8-sided die. If you roll any 2s, no points will be added and your score will decrease by 4. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 7, 7 points are added to your score).</li> <li> <b>By choosing Ten</b>:	you will roll a 10-sided die. If you roll any 2s, no points will be added and your score will decrease by 5. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 9, 9 points are added to your score).</li> <li><b>By choosing Twelve</b>: you will roll a 12-sided die. If you roll any 2s, no points will be added and your score will decrease by 6. Keep in mind, 12 has a 2 in it! If you roll either a 2 or a 12 your score will decrease by 6. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 11, 11 points are added to your score).</li> <li><b>By choosing Twenty</b>: you will roll a 20-sided die. If you roll a 2, no points will be added and your score will decrease by 8, Keep in mind, 12 and 20 both have 2s in them! If you roll either a 2, 12 or 20 your score will decrease by 8. Otherwise, the number on the die that you roll will be added to your total score (i.e. roll a 16, 16 points are added to your score).</li> </ul> You and the computer will take turns making a choice, and you will be up first. If you happen to reach 100 first, your opponent is allowed one last chance.";						
+	document.getElementById("introRules2").innerHTML = "I hope you're all ready to get started! <b>Click the 'Choose your opponent' button above on the right.</b>";												
 	return playerName;
 }																
 
@@ -75,7 +75,7 @@ function choseFour (){
 			fourScore = fourDie1 + fourDie2;  
 		}
 	playerScore += fourScore;
-	document.getElementById("listPlayer").innerHTML = ("You rolled a " + fourDie1 + " and a " + fourDie2 + " and earned " + fourScore + " points to your total.");
+	document.getElementById("listPlayer").innerHTML = ("You rolled a " + fourDie1 + " and a " + fourDie2 + " and earned " + fourScore + " point(s) to your total.");
 	updatePlayerScore(playerScore);
 	
 	opponentTurn(opponentScore);
@@ -92,7 +92,7 @@ function choseEight (){
 			eightScore = eightDie;  
 		}
 	playerScore += eightScore;
-	document.getElementById("listPlayer").innerHTML = ("You rolled an " + eightDie + " and earned " + eightScore + " points to your total.");
+	document.getElementById("listPlayer").innerHTML = ("You rolled an " + eightDie + " and earned " + eightScore + " point(s) to your total.");
 	updatePlayerScore(playerScore);
 	
 	opponentTurn(opponentScore);
@@ -109,7 +109,7 @@ function choseTen (){
 			tenScore = tenDie;  
 		}
 	playerScore += tenScore;
-	document.getElementById("listPlayer").innerHTML = ("You rolled an " + tenDie + " and earned " + tenScore + " points to your total.");
+	document.getElementById("listPlayer").innerHTML = ("You rolled an " + tenDie + " and earned " + tenScore + " point(s) to your total.");
 	updatePlayerScore(playerScore);
 	
 	opponentTurn(opponentScore);
@@ -120,13 +120,13 @@ function choseTwelve (){
 	var twelveDie = Math.floor(Math.random() * 12 + 1);
 	var twelveScore = 0;
 		if(twelveDie === 2 || twelveDie === 12){
-	        twelveScore = (-5);
+	        twelveScore = (-6);
 		} 
 		else {
 			twelveScore = twelveDie;  
 		}
 	playerScore += twelveScore;
-	document.getElementById("listPlayer").innerHTML = ("You rolled an " + twelveDie + " and earned " + twelveScore + " points to your total.");
+	document.getElementById("listPlayer").innerHTML = ("You rolled an " + twelveDie + " and earned " + twelveScore + " point(s) to your total.");
 	updatePlayerScore(playerScore);
 	
 	opponentTurn(opponentScore);
@@ -137,13 +137,13 @@ function choseTwenty (){
 	var twentyDie = Math.floor(Math.random() * 20 + 1);
 	var twentyScore = 0;
 		if(twentyDie === 2 || twentyDie === 12 || twentyDie === 20){
-	        twentyScore = (-6);
+	        twentyScore = (-8);
 		} 
 		else {
 			twentyScore = twentyDie;  
 		}
 	playerScore += twentyScore;
-	document.getElementById("listPlayer").innerHTML = ("You rolled an " + twentyDie + " and earned " + twentyScore + " points to your total.");
+	document.getElementById("listPlayer").innerHTML = ("You rolled an " + twentyDie + " and earned " + twentyScore + " point(s) to your total.");
 	updatePlayerScore(playerScore);
 	
 	opponentTurn(opponentScore);
@@ -201,7 +201,7 @@ function opponentFour (){
 			fourScore = fourDie1 + fourDie2;  
 		}	
 	opponentScore += fourScore;
-	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Four and rolled a " + fourDie1 + " and a " + fourDie2 + ". He just earned " + fourScore + " points added to his total.");
+	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Four and rolled a " + fourDie1 + " and a " + fourDie2 + ". He just earned " + fourScore + " point(s) added to his total.");
 	updateOpponentScore(opponentScore);
 	return opponentScore;
 }
@@ -217,7 +217,7 @@ function opponentEight (){
 			eightScore = eightDie;  
 		}
 	opponentScore += eightScore;
-	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Eight and rolled a " + eightDie + ". He just earned " + eightScore + " points added to his total.");
+	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Eight and rolled a " + eightDie + ". He just earned " + eightScore + " point(s) added to his total.");
 	updateOpponentScore(opponentScore);
 	return opponentScore;
 }	
@@ -233,7 +233,7 @@ function opponentTen (){
 			tenScore = tenDie;  
 		}
 	opponentScore += tenScore;
-	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Ten and rolled a " + tenDie + ". He just earned " + tenScore + " points added to his total.");
+	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Ten and rolled a " + tenDie + ". He just earned " + tenScore + " point(s) added to his total.");
 	updateOpponentScore(opponentScore);
 	return opponentScore;
 }
@@ -243,13 +243,13 @@ function opponentTwelve (){
 	var twelveDie = Math.floor(Math.random() * 12 + 1);
 	var twelveScore = 0;
 		if(twelveDie === 2 || twelveDie === 12){
-	        twelveScore = (-5);
+	        twelveScore = (-6);
 		} 
 		else {
 			twelveScore = twelveDie;  
 		}
 	opponentScore += twelveScore;
-	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Twelve and rolled a " + twelveDie + ". He just earned " + twelveScore + " points added to his total.");
+	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Twelve and rolled a " + twelveDie + ". He just earned " + twelveScore + " point(s) added to his total.");
 	updateOpponentScore(opponentScore);
 	return opponentScore;
 }														
@@ -259,13 +259,13 @@ function opponentTwenty (){
 	var twentyDie = Math.floor(Math.random() * 20 + 1);
 	var twentyScore = 0;
 		if(twentyDie === 2 || twentyDie === 12 || twentyDie === 20){
-	        twentyScore = (-6);
+	        twentyScore = (-8);
 		} 
 		else {
 			twentyScore = twentyDie;  
 		}
 	opponentScore += twentyScore;
-	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Twenty and rolled a " + twentyDie + ". He just earned " + twentyScore + " points added to his total.");
+	document.getElementById("listOpponent").innerHTML = ("Your opponent chose Twenty and rolled a " + twentyDie + ". He just earned " + twentyScore + " point(s) added to his total.");
 	updateOpponentScore(opponentScore);
 	return opponentScore;
 }											
